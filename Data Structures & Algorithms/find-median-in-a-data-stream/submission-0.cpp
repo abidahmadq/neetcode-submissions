8ad523981952f131c0dff94118453ee1b0c56d02
Abid Ahmad <abid.programming@gmail.com>
2026-06-15 00:22:18 +0530

    Add: find-median-in-a-data-stream - submission-0

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-0.cpp
@@ -0,0 +1,35 @@
+class MedianFinder {
+private:
+    priority_queue<int> small; // large heap
+    priority_queue<int, vector<int>, greater<int>> large; // min heap
+public:
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        small.push(num);
+        if(!large.empty() && small.top() > large.top()) {
+            large.push(small.top());
+            small.pop();
+        }  
+        if(small.size() > large.size() + 1) {
+            large.push(small.top());
+            small.pop();
+        }
+        if(large.size() > small.size() + 1) {
+            small.push(large.top());
+            large.pop();
+        }
+    }
+    
+    double findMedian() {
+        if(small.size() == large.size()) {
+            return (small.top() + large.top())/2.0;
+        } else if(small.size() > large.size()) {
+            return small.top();
+        } else {
+            return large.top();
+        }
+    }
+};
